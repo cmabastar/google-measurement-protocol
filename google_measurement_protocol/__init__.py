c61ffd8fc51fa0c8b438c3792655210111977f4b
cmabastar <marlon.abastar@gmail.com>
2019-07-09 01:11:26 +0800

    Removed items and transaction

--- google_measurement_protocol/__init__.py
+++ google_measurement_protocol/__init__.py
@@ -2,8 +2,6 @@ from .enhanced_purchase import enhanced_item, enhanced_purchase
 from .event import event
 from .pageview import pageview
 from .report import report
-from .transaction import item, transaction
 
 __all__ = [
-    'enhanced_item', 'enhanced_purchase', 'event', 'pageview', 'report',
-    'item', 'transaction']
+    'enhanced_item', 'enhanced_purchase', 'event', 'pageview', 'report']
